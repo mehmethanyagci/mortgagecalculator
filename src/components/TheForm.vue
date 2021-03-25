396357ed7d12fc0684baa08538cbd3950490ec05
MEHMETHAN YAGCI <36566756+mehmethanyagci@users.noreply.github.com>
2021-03-24 22:20:09 -0400

    Add files via upload

--- src/components/TheForm.vue
+++ src/components/TheForm.vue
@@ -4,73 +4,79 @@
     <h3>Estimate how much you'll pay each month for your new home with our easy-to-use mortgage calculator.</h3>
     <div class="two">
       <form @submit.prevent="calculate">
-        <div id="sample-form">
+        <div>
           <div>
-            <div>
-              <label for="Home price" class="labels">
-                <strong>Home price</strong>
-              </label>
-            </div>
-            <input
-              type="number"
-              name="homePrice"
-              id="homePrice"
-              placeholder="purchase price"
-              v-model.number="homePrice"
-              required
-            />
+            <label for="Home price" class="labels">
+              <strong>Home price</strong>
+            </label>
           </div>
-          <div>
-            <div>
-              <label for="Home price" class="labels">
-                <strong>Down payment</strong>
-              </label>
-            </div>
-            <input
-              type="number"
-              name="downPayment"
-              id="downPayment"
-              placeholder="down payment"
-              v-model.number="downPayment"
-              required
-            />
-          </div>
-          <div>
-            <div>
-              <label for="Home price" class="labels">
-                <strong>Loan program</strong>
-              </label>
-            </div>
-            <select
-              type="check"
-              name="loanProgram"
-              id="loanProgram"
-              placeholder="loan"
-              v-model.number="loanProgram"
-              required
-            >
-              <option value="15">15-year-fixed</option>
-              <option value="30">30-year-fixed</option>
-            </select>
-          </div>
-          <div>
-            <div>
-              <label for="Home price" class="labels">
-                <strong>Interest rate</strong>
-              </label>
-            </div>
-            <input
-              type="number"
-              name="interestRate"
-              id="interestRate"
-              placeholder="interest rate in percentage"
-              v-model="interestRate"
-              step="any"
-              required
-            />
-          </div>
+          <input
+            type="number"
+            name="homePrice"
+            id="homePrice"
+            placeholder="purchase price"
+            v-model.number="homePrice"
+            required
+          />
         </div>
-        <!-- <div id="advanced-form" v-if="showAdvanced"> -->
+        <div v-if="!validHomePrice" class="warning">Please enter a valid home price greater than 0.</div>
+        <div>
+          <div>
+            <label for="Home price" class="labels">
+              <strong>Down payment</strong>
+            </label>
+          </div>
+          <input
+            type="number"
+            name="downPayment"
+            id="downPayment"
+            placeholder="down payment"
+            v-model.number="downPayment"
+            required
+          />
+        </div>
+        <div v-if="!validDownPayment" class="warning">
+          <p>Please enter a valid down payment greater than or equal to 0.</p>
+          <p>Most lenders require at least 3.5 percent of the home price.</p>
+        </div>
+        <div>
+          <div>
+            <label for="Home price" class="labels">
+              <strong>Loan program</strong>
+            </label>
+          </div>
+          <select
+            type="check"
+            name="loanProgram"
+            id="loanProgram"
+            placeholder="loan"
+            v-model.number="loanProgram"
+            required
+          >
+            <option value="15">15-year-fixed</option>
+            <option value="30">30-year-fixed</option>
+          </select>
+        </div>
+        <div>
+          <div>
+            <label for="Home price" class="labels">
+              <strong>Interest rate</strong>
+            </label>
+          </div>
+          <input
+            type="number"
+            name="interestRate"
+            id="interestRate"
+            placeholder="interest rate in percentage"
+            v-model="interestRate"
+            step="any"
+            required
+          />
+        </div>
+        <div
+          v-if="!validInterestRate"
+          class="warning"
+        >Please enter a valid interest rate greater than or equal to 0.</div>
         <div>
           <div>
             <label for="taxRate" class="labels">
@@ -87,6 +93,10 @@
             required
           />
         </div>
+        <div
+          v-if="!validTaxRate"
+          class="warning"
+        >Please enter a valid property tax rate greater than or equal to 0.</div>
         <div>
           <div>
             <label for="taxRate" class="labels">
@@ -101,20 +111,20 @@
             v-model.number="homeInsurance"
           />
         </div>
+        <div
+          v-if="!validHomeInsurance"
+          class="warning"
+        >Please enter a valid home insurance greater than or equal to 0.</div>
         <div>
           <div>
-            <label for="taxRate" class="labels">
+            <label for="HOA" class="labels">
               <strong>HOA</strong>
             </label>
           </div>
-          <input type="number" name="hoa" id="hoa" placeholder="hoa" v-model.number="hoa" />
+          <input type="number" name="HOA" id="HOA" placeholder="HOA" v-model.number="HOA" />
         </div>
-        <!-- </div> -->
-        <!-- <button type="submit" id="submitButton">Submit</button> -->
+        <div v-if="!validHOA" class="warning">Please enter a valid HOA greater than or equal to 0.</div>
       </form>
-      <!-- <button @click="toggle" id="toggleButton">{{!showAdvanced ? 'Show Advanced ' : 'Show Simple' }}</button> -->
-
-      <!-- <the-visualization :dataObj="dataObj"></the-visualization> -->
       <div class="dataviz">
         <h2>
           <strong>Your payment breakdown</strong>
@@ -122,7 +132,7 @@
         <p>Monthly payment: ${{Math.round(dataObj.monthlyPayment)}}</p>
         <p>Principal and interest: ${{Math.round(dataObj.principalAndInterest)}}</p>
         <p>Taxes: ${{Math.round(dataObj.taxes/12)}}</p>
-        <p>HOA: ${{Math.round(dataObj.hoa)}}</p>
+        <p>HOA: ${{ Math.round(dataObj.HOA) ? Math.round(dataObj.HOA) : 0 }}</p>
         <p>Insurance: ${{Math.round(dataObj.insurance/12)}}</p>
       </div>
     </div>
@@ -130,12 +140,7 @@
 </template>
 
 <script>
-//import TheVisualization from "./TheVisualization.vue";
-
 export default {
-  // components: {
-  //   TheVisualization
-  // },
   data() {
     return {
       showAdvanced: false,
@@ -145,10 +150,10 @@ export default {
       interestRate: 3.0,
       i: this.interestRate / 1200,
       n: this.loanProgram * 12,
-      taxRate: 3.0,
+      taxRate: 3,
       propertyTax: null,
       homeInsurance: 500,
-      hoa: 42,
+      HOA: 42,
       principalAndInterest: null,
       monthlyPayment: null,
       dataObj: {
@@ -157,7 +162,13 @@ export default {
         taxes: this.propertyTax,
         hoa: this.hoa,
         insurance: this.homeInsurance
-      }
+      },
+      validHomePrice: true,
+      validDownPayment: true,
+      validInterestRate: true,
+      validTaxRate: true,
+      validHomeInsurance: true,
+      validHOA: true
     };
   },
   methods: {
@@ -165,28 +176,79 @@ export default {
       this.showAdvanced = !this.showAdvanced;
     },
     calculate() {
-      this.i = this.interestRate / 1200;
+      if (this.homePrice <= 0) {
+        this.validHomePrice = false;
+      }
 
-      this.n = this.loanProgram * 12;
+      if (this.downPayment < 0) {
+        this.validDownPayment = false;
+      }
 
-      this.propertyTax = (this.homePrice * this.taxRate) / 100;
+      if (this.interestRate < 0) {
+        this.validInterestRate = false;
+      }
 
-      this.principalAndInterest =
-        ((this.homePrice - this.downPayment) *
-          (this.i * Math.pow(this.i + 1, this.n))) /
-        (Math.pow(1 + this.i, this.n) - 1);
+      if (this.taxRate < 0) {
+        this.validTaxRate = false;
+      }
 
-      this.monthlyPayment =
-        this.principalAndInterest +
-        this.homeInsurance / 12 +
-        this.hoa +
-        this.propertyTax / 12;
+      if (this.homeInsurance < 0) {
+        this.validHomeInsurance = false;
+      }
 
-      this.dataObj.principalAndInterest = this.principalAndInterest;
-      this.dataObj.monthlyPayment = this.monthlyPayment;
-      this.dataObj.taxes = this.propertyTax;
-      this.dataObj.hoa = this.hoa;
-      this.dataObj.insurance = this.homeInsurance;
+      if (this.HOA < 0) {
+        this.validHOA = false;
+      }
+
+      if (this.HOA === "") {
+        this.validHOA = false;
+      }
+
+      if (
+        this.homePrice > 0 &&
+        this.downPayment > 0 &&
+        this.interestRate >= 0 &&
+        this.taxRate >= 0 &&
+        this.homeInsurance >= 0 &&
+        this.HOA >= 0 &&
+        this.HOA !== ""
+      ) {
+        this.validHomePrice = true;
+        this.validDownPayment = true;
+        this.validInterestRate = true;
+        this.validTaxRate = true;
+        this.validHomeInsurance = true;
+        this.validHOA = true;
+
+        this.i = this.interestRate / 1200;
+
+        this.n = this.loanProgram * 12;
+
+        this.propertyTax = (this.homePrice * this.taxRate) / 100;
+
+        this.principalAndInterest =
+          ((this.homePrice - this.downPayment) *
+            (this.i * Math.pow(this.i + 1, this.n))) /
+          (Math.pow(1 + this.i, this.n) - 1);
+
+        this.monthlyPayment =
+          this.principalAndInterest +
+          this.homeInsurance / 12 +
+          this.HOA +
+          this.propertyTax / 12;
+
+        this.dataObj.principalAndInterest = this.principalAndInterest;
+        this.dataObj.monthlyPayment = this.monthlyPayment;
+        this.dataObj.taxes = this.propertyTax;
+        this.dataObj.HOA = this.HOA;
+        this.dataObj.insurance = this.homeInsurance;
+      } else {
+        this.dataObj.principalAndInterest = 0;
+        this.dataObj.monthlyPayment = 0;
+        this.dataObj.taxes = 0;
+        this.dataObj.HOA = 0;
+        this.dataObj.insurance = 0;
+      }
     }
   },
   mounted() {
@@ -199,6 +261,7 @@ export default {
 </script>
 
 <style scoped>
+
 p {
   font-size: 1rem;
 }
@@ -220,6 +283,11 @@ form {
   min-width: 30rem;
 }
 
+.warning {
+  color: red;
+  font-weight: bolder;
+}
+
 .dataviz {
   margin: 3rem 1rem 1rem 0rem;
 }
@@ -233,8 +301,7 @@ div {
   flex-direction: row;
 }
 
-input,
-select {
+input, select {
   border-radius: 6px;
   font-size: 1rem;
   padding: 0.4rem 0.4rem;
@@ -246,22 +313,7 @@ h1 {
   text-align: center;
 }
 
-#toggleButton {
-  margin-left: 12rem;
-  cursor: pointer;
-}
-
-#submitButton {
-  margin-left: 0rem;
-  cursor: pointer;
-  margin-top: 1rem;
-  border-radius: 6px;
-  width: 5rem;
-  color: white;
-  background-color: rgb(25, 71, 257);
-}
-
 .dataviz p {
-  font-weight: bold;
+  font-weight: bolder;
 }
 </style>
